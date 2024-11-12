0c07b2b9c11ac540fc7d5559f9bc9dd4e2a24567
Yoonseok Hur <yoonseok@gmail.com>
2024-11-12 12:23:19 +0900

    init

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include <ConfigPortal32.h>
+#include <PubSubClient.h>
+
+char*               ssid_pfix = (char*)"mqtt_relay_";
+String              user_config_html = ""
+    "<p><input type='text' name='broker' placeholder='InfluxDB Address'>";
+
+char                mqttServer[100];
+ 
+#define             RELAY 15
+const int           mqttPort = 1883;
+
+unsigned long       interval = 10000;
+unsigned long       lastPublished = - interval;
+ 
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+void msgCB(char* topic, byte* payload, unsigned int length); // message Callback
+void pubStatus();
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(RELAY, OUTPUT);
+
+    loadConfig();
+    // *** If no "config" is found or "config" is not "done", run configDevice ***
+    if(!cfg.containsKey("config") || strcmp((const char*)cfg["config"], "done")) {
+        configDevice();
+    }
+    WiFi.mode(WIFI_STA);
+    WiFi.begin((const char*)cfg["ssid"], (const char*)cfg["w_pw"]);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    // main setup
+    Serial.printf("\nIP address : "); Serial.println(WiFi.localIP());
+
+    if (cfg.containsKey("broker")) {
+            sprintf(mqttServer, (const char*)cfg["broker"]);
+    }
+    client.setServer(mqttServer, mqttPort);
+    client.setCallback(msgCB);
+
+    while (!client.connected()) {
+        Serial.println("Connecting to MQTT...");
+        if (client.connect("ESP8266Relay")) {
+            Serial.println("connected");  
+        } else {
+            Serial.print("failed with state "); Serial.println(client.state());
+            delay(2000);
+        }
+    }
+ 
+    client.subscribe("id/yourname/relay/cmd");
+    digitalWrite(RELAY, LOW);
+}
+
+void loop() {
+    client.loop();
+
+    unsigned long currentMillis = millis();
+    if(currentMillis - lastPublished >= interval) {
+        lastPublished = currentMillis;
+        pubStatus();
+    }
+}
+
+void pubStatus() {
+    char buf[10];
+    if (digitalRead(RELAY) == HIGH) {
+        sprintf(buf, "on");
+    } else {
+        sprintf(buf, "off");
+    }
+    client.publish("id/yourname/relay/evt", buf);
+}
+
+void msgCB(char* topic, byte* payload, unsigned int length) {
+ 
+    char msgBuffer[20];
+    if(!strcmp(topic, "id/yourname/relay/cmd")) {
+        int i;
+        for(i = 0; i < (int)length; i++) {
+            msgBuffer[i] = payload[i];
+        } 
+        msgBuffer[i] = '\0';
+        Serial.printf("\n%s -> %s", topic, msgBuffer);
+        if(!strcmp(msgBuffer, "on")) {
+            digitalWrite(RELAY, HIGH);
+        } else if(!strcmp(msgBuffer, "off")) {
+            digitalWrite(RELAY, LOW);
+        }
+    }
+    pubStatus();
+}
