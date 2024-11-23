ea214e8c482307ed524168f43e9e3d46d057f636
ParkJongHa <hikk31784@tukorea.ac.kr>
2024-11-23 11:36:37 +0900

    1

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Plain MQTT Realy Example
 
 
 <img width="416" alt="Screenshot 2024-10-29 at 4 05 15 PM" src="https://github.com/user-attachments/assets/83b18c6f-af39-45b9-96af-1c68a416eecc">
+"# team_NeoPixel_press" 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <ConfigPortal32.h>
 #include <PubSubClient.h>
+#include <Adafruit_NeoPixel.h>
 
-char*               ssid_pfix = (char*)"mqtt_relay_";
+char*               ssid_pfix = (char*)"NeoPixel_FSR";
 String              user_config_html = ""
     "<p><input type='text' name='broker' placeholder='MQTT Server'>";
 
 char                mqttServer[100];
- 
-#define             RELAY 15
 const int           mqttPort = 1883;
 
 unsigned long       interval = 10000;
@@ -18,6 +17,33 @@ PubSubClient client(wifiClient);
 void msgCB(char* topic, byte* payload, unsigned int length); // message Callback
 void pubStatus();
 
+///////////////////////////////////////////////
+
+int FSRsensor = A0; // A0에 FSR 센서 연결
+
+#define ledPinA 15 
+#define ledNumA 8
+#define ledPinB 2
+#define ledNumB 24
+Adafruit_NeoPixel pixelsA(ledNumA, ledPinA, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixelsB(ledNumB, ledPinB, NEO_GRB + NEO_KHZ800);
+
+void waterDropA(int i, int R, int G, int B, int maxTail = 4, int tail = 1) {
+    int divider = exp(tail);
+    pixelsA.setPixelColor(i, pixelsA.Color(R/divider, G/divider, B/divider));
+    if (i < ledNumA && tail < maxTail) {
+        waterDropA(++i, R, G, B, maxTail, ++tail);
+    }
+}
+void waterDropB(int i, int R, int G, int B, int maxTail = 4, int tail = 1) {
+    int divider = exp(tail);
+    pixelsB.setPixelColor(i, pixelsB.Color(R/divider, G/divider, B/divider));
+    if (i < ledNumB && tail < maxTail) {
+        waterDropB(++i, R, G, B, maxTail, ++tail);
+    }
+}
+
+///////////////////////////////////////////////
 
 void loop() {
     client.loop();
@@ -25,13 +51,15 @@ void loop() {
     unsigned long currentMillis = millis();
     if(currentMillis - lastPublished >= interval) {
         lastPublished = currentMillis;
-        pubStatus();
+        pubStatus(); // 압력 센서 정보 pub
     }
 }
 
 void setup() {
     Serial.begin(115200);
-    pinMode(RELAY, OUTPUT);
+
+    pixelsA.begin();
+    pixelsB.begin();
 
     loadConfig();
     // *** If no "config" is found or "config" is not "done", run configDevice ***
@@ -51,7 +79,7 @@ void setup() {
             sprintf(mqttServer, (const char*)cfg["broker"]);
     }
     client.setServer(mqttServer, mqttPort);
-    client.setCallback(msgCB);
+    client.setCallback(msgCB); // 메시지 callback - neopixel topic sub
 
     while (!client.connected()) {
         Serial.println("Connecting to MQTT...");
@@ -63,35 +91,59 @@ void setup() {
         }
     }
  
-    client.subscribe("id/yourname/relay/cmd");
-    digitalWrite(RELAY, LOW);
+    client.subscribe("id/yourname/NeoPixel/cmd"); // 받을 메시지 설정
 }
 
-void pubStatus() {
+void pubStatus() 
+{
+    int value;
     char buf[10];
-    if (digitalRead(RELAY) == HIGH) {
-        sprintf(buf, "on");
-    } else {
-        sprintf(buf, "off");
-    }
-    client.publish("id/yourname/relay/evt", buf);
+
+    value= analogRead(FSRsensor);
+    Serial.println(value);
+    //value = map(value, 0, 1023, 0, 255); 
+    sprintf(buf, "%d", value);
+    client.publish("id/yourname/sensor/evt/press", buf);
 }
 
-void msgCB(char* topic, byte* payload, unsigned int length) {
- 
+void msgCB(char* topic, byte* payload, unsigned int length) 
+{
     char msgBuffer[20];
-    if(!strcmp(topic, "id/yourname/relay/cmd")) {
+    if(!strcmp(topic, "id/yourname/NeoPixel/cmd")) {
         int i;
         for(i = 0; i < (int)length; i++) {
             msgBuffer[i] = payload[i];
         } 
         msgBuffer[i] = '\0';
         Serial.printf("\n%s -> %s", topic, msgBuffer);
-        if(!strcmp(msgBuffer, "on")) {
-            digitalWrite(RELAY, HIGH);
-        } else if(!strcmp(msgBuffer, "off")) {
-            digitalWrite(RELAY, LOW);
+        if(!strcmp(msgBuffer, "box")) 
+        {
+            int R = random(0, 255);
+            int G = random(0, 255);
+            int B = random(0, 255);
+
+            for (int i = ledNumB-1; i >= 0 ; i--) 
+            {
+                pixelsB.clear();
+                waterDropB(i, R, G, B);
+                pixelsB.show();
+                delay(100);
+            }
+        } 
+        else if(!strcmp(msgBuffer, "door")) 
+        {
+            int R = random(0, 255);
+            int G = random(0, 255);
+            int B = random(0, 255);
+
+            for (int i = ledNumA-1; i >= 0 ; i--) 
+            {
+                pixelsA.clear();
+                waterDropA(i, R, G, B);
+                pixelsA.show();
+                delay(100);
+            }
         }
     }
-    pubStatus();
+    //pubStatus();
 }
